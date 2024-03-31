f398b200a52faa11ef791aefb2aa5b26a8732bcd
Anton Engelhardt <anton.engelhardt@anduro.de>
2024-03-31 21:22:36 +0000

    added ipp example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import { $ } from 'zx'
+import { $, fs } from 'zx'
+import ipp from "ipp";
+
 const date = await $`date`
 await $`echo Current date is ${date}.`
+
+const pdf = fs.readFileSync('myfile.pdf', "binary");
+
+const printer = new ipp.Printer("http://NPI977E4E.local.:631/ipp/printer")
+var msg = {
+    "operation-attributes-tag": {
+        "requesting-user-name": "William",
+        "job-name": "My Test Job",
+        "document-format": "application/pdf"
+    },
+    data: pdf
+};
+//was Print-Job
+printer.execute("Create-Job", msg, function (err, res) {
+    console.log(res);
+});
